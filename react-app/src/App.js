7e0860375f1b064e192a216631ce99aa00b38d8c
jyojun <parkcode98@gmail.com>
2021-05-09 02:03:12 +0900

    bind, setState

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Subject from "./components/Subject";
+import Content from "./components/Contents";
+import TOC from "./components/TOC";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props){ // component가 실행될 때 constructor함수가 제일 먼저 실행되어서 초기화를 담당 
+    super(props);
+    this.state = {
+      mode:'read',
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaSciprt', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
+  render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    return (
+      <div className="App">
+        {/* <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}> 
+        </Subject> */}
+        <header>
+          <h1><a href="/" onClick={function(e){
+            console.log(e);
+            e.preventDefault(); // 자동 reload 를 막아줌
+            // this.state.mode = 'welcome'; // 이렇게 하면 react가 state가 변한것을 모름.
+            this.setState({
+              mode:'welcome'
+            });
+          }.bind(this)}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
+      </div>
+    );
+  }
 }
+
 
 export default App;
