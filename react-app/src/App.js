17f915152036ebead760fe573b2b372f77590ac5
jyojun <parkcode98@gmail.com>
2021-05-14 21:33:36 +0900

    update, delete 구현

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,43 +2,91 @@ import React, { Component } from 'react';
 import Subject from "./components/Subject";
 import Control from "./components/Control";
 import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
+import UpdateContent from "./components/UpdateContent";
 import TOC from "./components/TOC";
 import './App.css';
-
 
 class App extends Component {
   constructor(props){ // component가 실행될 때 constructor함수가 제일 먼저 실행되어서 초기화를 담당 
     super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode:'read',
+      mode:'create',
       selected_content_id:2,
       subject:{title:'WEB', sub:'World Wide Web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
-        {id:3, title:'JavaSciprt', desc:'JavaScript is for interactive'}
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
       ]
     }
   }
-  render() {
-    //console.log('App render');
-    var _title, _desc = null;
+  getReadContent() {
+    var i = 0;
+    while(i < this.state.contents.length){
+      var data = this.state.contents[i];
+      if(data.id === this.state.selected_content_id) {
+        return data;
+        //break;
+      }
+      i = i + 1;
+    }
+  }
+  getContent() {
+    console.log('App render');
+    var _title, _desc, _article = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if(this.state.mode === 'read'){
-      var i = 0;
-      while(i < this.state.contents.length){
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i + 1;
-      }
+        var _content = this.getReadContent();
+        _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
+    } else if(this.state.mode === 'create'){
+      _article = <CreateContent onSubmit={function(_title, _desc){ // _title, _desc가 function 안에서 값이 바뀜
+        // add content to this.state.contents
+        this.max_content_id = this.max_content_id+1;
+        // this.state.contents.push(
+        //   {id:this.max_content_id, title: _title, desc: _desc}
+        // );
+        // var _contents = this.state.contents.concat(
+        //   {id:this.max_content_id, title: _title, desc: _desc}
+        // ); // concat은 원본 데이터를 바꾸지 않는다! 
+        var newContents = Array.from(this.state.contents); // 객체는 Object.assign({}, contents);
+        newContents.push({id:this.max_content_id, title: _title, desc: _desc});
+        this.setState({ // 바뀐 state content 값을 setState로 리액트에게 알려줌!
+          contents:newContents,
+          mode:'read',
+          selected_content_id:this.max_content_id
+        });
+        console.log(_title, _desc);
+      }.bind(this)}></CreateContent>
+    } else if(this.state.mode === 'update'){
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc){
+          var _contents = Array.from(this.state.contents);
+          var i = 0;
+          while(i < _contents.length){
+            if(_contents[i].id === _id) {
+              _contents[i] = {id:_id, title:_title, desc:_desc};
+              break; // 끝나면 더이상 돌지 않음
+            }
+            i = i + 1;
+          }
+          this.setState({ // 바뀐 state content 값을 setState로 리액트에게 알려줌!
+            contents:_contents,
+            mode:'read'
+          });
+          console.log(_title, _desc);
+      }.bind(this)}></UpdateContent>
     }
+    return _article;
+  }
+  render() {
+    console.log('App render');
     return (
       <div className="App">
         <Subject 
@@ -59,13 +107,29 @@ class App extends Component {
           data={this.state.contents}
         ></TOC>
         <Control onChangeMode={function(_mode){
-          this.setState({
-            mode:_mode
-          })
+          if(_mode === 'delete'){
+            if(window.confirm('Really?')){
+              var _contents = Array.from(this.state.contents);
+              var i = 0;
+              while(i < _contents.length) {
+                if(_contents[i].id === this.state.selected_content_id){
+                  _contents.splice(i, 1);
+                  break;
+                }
+                i = i + 1;
+              }
+              this.setState({
+                mode:'welcome',
+                contents:_contents
+              })
+            }
+          } else {
+            this.setState({
+              mode:_mode
+            });
+          }
         }.bind(this)}></Control>
-
-
-        <Content title={_title} desc={_desc}></Content>
+        {this.getContent()}
       </div>
     );
   }
--- react-app/src/components/TOC.js
+++ react-app/src/components/TOC.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 
 class TOC extends Component {
+    shouldComponentUpdate(newProps, newState) {
+      console.log('TOC render shouldComponentUpdate',
+      newProps.data,
+      this.props.data
+      );
+
+      // concat이 아니라 push로 리스트를 추가했다면 밑과 같은 방식은 할 수 없다. (원본데이터가 바뀌었기 때문에, newProps === this.props 가 되버립)
+      if(this.props.data === newProps.data){ // 바뀐값이 없으면 render 함수를 호출하지 않음
+        return false; // false 면 밑의 render 함수는 호출하지 x
+      }
+      return true; 
+    }
     render() {
+      console.log('TOC render');
       var lists = [];
       var data = this.props.data;
       var i = 0;
